717f1dd4657d2cf1fe54771fa78a7df920621205
PatriziaCrimi <crimi.patrizia@gmail.com>
2022-02-02 14:44:48 +0100

    Snack 3 - Function to generate random numbers added

--- php-snack3/index.php
+++ php-snack3/index.php
@@ -1,18 +1,81 @@
 <?php
   /* ISTRUZIONI
-  Generazione di 5 numeri casuali.
+  Generazione di 5 numeri casuali interi compresi tra 0 e 5000.
   Il programma stampa la somma di tutti i numeri.
   */
 
   // Variables initialization
+  $title = 'PHP Snack 3';
+  $subtitle = 'Summing of numbers';
+  $numbers_quantity = 5;
+  $numbers_array = [];
+  $min = 0;
+  $max = 5000;
+  for ($i=0; $i < $numbers_quantity; $i++) {
+    $random_number = rand($min, $max);
+    $numbers_array[] = $random_number;
+  }
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title>PHP Snack 3</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+    <link rel="stylesheet" href="../style.css">
   </head>
   <body>
-
+    <div id="page-wrapper">
+      <div class="container">
+        <div class="row">
+          <h1>
+            <?php echo $title; ?>
+          </h1>
+        </div>
+        <div class="row">
+          <h2>
+            <?php echo $subtitle; ?>
+          </h2>
+        </div>
+        <div class="row">
+          <!-- Printing data -->
+          <div id="data-info">
+            <h3>Data Information</h3>
+            <p>
+              The five numbers randomly generated are:
+              <?php
+                for ($i=0; $i < count($numbers_array); $i++) {
+                  echo $numbers_array[$i];
+                }
+              ?>
+            </p>
+          </div>
+        </div>
+        <div class="row">
+          <!-- Results -->
+          <div id="results">
+            <h3>Results</h3>
+            <div class="message">
+              <p>
+                MESSAGE HERE
+              </p>
+            </div>
+            <div class="results-box">
+              <p>
+                <span>
+                  RESULTS BOX:
+                </span>
+                <?php
+                  echo 'CONTENT HERE';
+                ?>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>  <!-- End of container -->
+    </div>  <!-- End of page wrapper -->
   </body>
 </html>
